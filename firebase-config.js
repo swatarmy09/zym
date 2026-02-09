db30f4f555c6da33a52b993c4c53b96a2bef438e
Mehul Patel <swatmehul67@gmail.com>
2026-02-09 23:31:20 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -8,14 +8,15 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
 
-  apiKey: "AIzaSyAByrfdGPEUm_3_Wv7PMt31O9PPjsE6TA0",
-  authDomain: "otpl-357f7.firebaseapp.com",
-  databaseURL: "https://otpl-357f7-default-rtdb.firebaseio.com",
-  projectId: "otpl-357f7",
-  storageBucket: "otpl-357f7.firebasestorage.app",
-  messagingSenderId: "520555173842",
-  appId: "1:520555173842:web:e146c05a90001b45505a0b",
-  measurementId: "G-LF40EWLJ31"
+const firebaseConfig = {
+    apiKey: "AIzaSyAByrfdGPEUm_3_Wv7PMt31O9PPjsE6TA0",
+    authDomain: "otpl-357f7.firebaseapp.com",
+    databaseURL: "https://otpl-357f7-default-rtdb.firebaseio.com",
+    projectId: "otpl-357f7",
+    storageBucket: "otpl-357f7.firebasestorage.app",
+    messagingSenderId: "520555173842",
+    appId: "1:520555173842:web:e146c05a90001b45505a0b",
+    measurementId: "G-LF40EWLJ31"
 };
 
 // Initialize Firebase
@@ -24,4 +25,3 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 
 console.log('🔥 Firebase initialized successfully');
-
